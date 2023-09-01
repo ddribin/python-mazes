c52606d0788e9b1240b8b89ec1fc2144fd9eb5a2
Dave Dribin <dave-vcs@dribin.org>
2023-08-31 23:38:05 -0500

    Implement Dijkstra and test it

--- src/mazes/algorithms/dijkstra.py
+++ src/mazes/algorithms/dijkstra.py
@@ -6,11 +6,58 @@ from collections.abc import Iterator
 class Dijkstra:
     def __init__(self, grid: ImmutableGrid, root: Coordinate) -> None:
         self._grid = grid
+        self._distances = Distances(grid.width, grid.height)
         self._root = root
-        self._max = 0
+        self._max_distance = 0
+        self._max_coordinate = root
+
+    @property
+    def distances(self) -> Distances:
+        return self._distances
+
+    @property
+    def max_distance(self) -> int:
+        return self._max_distance
+
+    @property
+    def max_coordinate(self) -> Coordinate:
+        return self._max_coordinate
 
     def steps(self) -> Iterator[None]:
         yield
 
     def generate(self) -> None:
-        pass
+        distances = self._distances
+        root = self._root
+        grid = self._grid
+
+        distances[root] = 0
+        frontier = [root]
+        max_coord = root
+        max_distance = 0
+
+        while frontier:
+            new_frontier: list[Coordinate] = []
+
+            for current in frontier:
+                linked = grid[current]
+                if linked is not None:
+                    distance = distances[current]
+                    assert distance is not None
+
+                    for direction in linked:
+                        next_coord = direction.update_coordinate(current)
+                        next_distance = distances[next_coord]
+                        if next_distance is not None:
+                            continue
+                        next_distance = distance + 1
+                        distances[next_coord] = next_distance
+                        new_frontier.append(next_coord)
+                        if next_distance > max_distance:
+                            max_coord = next_coord
+                            max_distance = next_distance
+
+            frontier = new_frontier
+
+        self._max_distance = max_distance
+        self._max_coordinate = max_coord
--- tests/algorithms/dijkstra_test.py
+++ tests/algorithms/dijkstra_test.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 from mazes.algorithms import Dijkstra
 from mazes.grid import Grid, ImmutableGrid, Direction as D
 from mazes.renderers import TextRenderer
@@ -8,7 +10,24 @@ from ..asserts import *
 class TestDijkstra:
     def test_dijstra(self):
         grid = self.make_grid()
-        assert grid is not None
+        dijkstra = Dijkstra(grid, (0, 0))
+
+        dijkstra.generate()
+
+        expected = [
+            [0, 1, 2, 9],
+            [7, 4, 3, 8],
+            [6, 5, 6, 7],
+            [7, 8, 7, 8],
+        ]
+
+        distances = dijkstra.distances
+        for y in range(4):
+            for x in range(4):
+                assert distances[x, y] == expected[y][x], f"({x=}, {y=})"
+
+        assert dijkstra.max_distance == 9
+        assert dijkstra.max_coordinate == (3, 0)
 
     def make_grid(self) -> ImmutableGrid:
         grid = Grid(4, 4)
@@ -18,6 +37,9 @@ class TestDijkstra:
 
         coord = grid.link_path((1, 2), [D.W, D.N])
         assert coord == (0, 1)
+
+        coord = grid.link_path((0, 2), [D.S])
+        assert coord == (0, 3)
 
         coord = grid.link_path((1, 2), [D.E])
         assert coord == (2, 2)
@@ -31,8 +53,8 @@ class TestDijkstra:
         coord = grid.link_path((2, 3), [D.E])
         assert coord == (3, 3)
 
-        coord = grid.link_path((2, 3), [D.W, D.W])
-        assert coord == (0, 3)
+        coord = grid.link_path((2, 3), [D.W])
+        assert coord == (1, 3)
 
         expected = (
             #  0   1   2   3
@@ -42,8 +64,8 @@ class TestDijkstra:
             "|   |       |   |\n"  # 1
             "+   +   +---+   +\n"
             "|               |\n"  # 2
-            "+---+---+   +---+\n"
-            "|               |\n"  # 3
+            "+   +---+   +---+\n"
+            "|   |           |\n"  # 3
             "+---+---+---+---+\n"
         )
         text = TextRenderer.render_grid(grid)
